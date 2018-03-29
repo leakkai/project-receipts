077f7e52a2a01fd65295b6aa42f33cf9926aeacc
Kai <leakkai@gmail.com>
2018-03-29 10:05:45 -0500

    Detail UI in construction pt2

--- gs-validating-form-input-complete/src/main/resources/static/script/script.js
+++ gs-validating-form-input-complete/src/main/resources/static/script/script.js
@@ -1,3 +1,25 @@
 flatpickr(".date", {
     enableTime: true,
 });
+
+var $TABLE = $('#table');
+
+$('.table-add').click(function () {
+  var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+  $TABLE.find('table').append($clone);
+});
+
+$('.table-remove').click(function () {
+  $(this).parents('tr').detach();
+});
+
+$('.table-up').click(function () {
+  var $row = $(this).parents('tr');
+  if ($row.index() === 1) return; // Don't go above the header
+  $row.prev().before($row.get(0));
+});
+
+$('.table-down').click(function () {
+  var $row = $(this).parents('tr');
+  $row.next().after($row.get(0));
+});
